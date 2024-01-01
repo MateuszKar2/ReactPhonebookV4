e262b1983bb44ba115f31980d66c4d6d0d01e6aa
MateuszKar2 <system505@protonmail.com>
2024-01-01 22:26:05 +0100

    add file - redux

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './ContactList.module.css'
 import ContactItem from 'components/ContactItem/ContactItem';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactSlice';
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = ({ contacts }) => {
+
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
     return (
       <ul className={styles.list}>
         {contacts.map(({ id, name, number }) => (
@@ -11,7 +20,7 @@ const ContactList = ({ contacts, onDeleteContact }) => {
             key={id}
             name={name}
             number={number}
-            onDeleteContact={() => onDeleteContact(id)}
+            onDeleteContact={() => handleDeleteContact(id)}
           />
         ))}
       </ul>
