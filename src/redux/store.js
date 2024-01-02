b61b244f773ac7f728914727a5fb090bf1854755
MateuszKar2 <system505@protonmail.com>
2024-01-02 22:55:20 +0100

    add file

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import contactReducer from "./contactSlice";
 import filterReducer from "./filterSlice";
 
-
 const store = configureStore({
-    reducer: {
-        contacts: contactReducer,
-        filter: filterReducer
-    }
+  reducer: {
+    contacts: contactReducer,
+    filter: filterReducer
+  }
 });
 
 export default store;
