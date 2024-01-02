9044481801767224340f8b5c008e1f486d69865b
MateuszKar2 <system505@protonmail.com>
2024-01-02 22:55:14 +0100

    add file

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,19 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const contactSlice = createSlice({
-    name: 'contacts',
-    initialState: [],
-    reducers: {
-        addContact: (state, action) => {
-            state.push(action.payload)
-        },
-        deleteContact: (state, action) => {
-            return state.filter(contact => contact.id !== action.payload)
-        }
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: (state, action) => {
+      state.push(action.payload)
+    },
+    deleteContact: (state, action) => {
+      return state.filter(contact => contact.id !== action.payload)
     }
+  }
 });
-
 
 export const { addContact, deleteContact } = contactSlice.actions;
 export default contactSlice.reducer;
