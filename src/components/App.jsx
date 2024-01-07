2b1b9a063b9715ae3fa25825393de8809efd8250
MateuszKar2 <system505@protonmail.com>
2024-01-07 23:23:17 +0100

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
+import { Provider } from 'react-redux/es';
+import store from "../redux/store"
 import Phonebook from './Phonebook/Phonebook';
-import { Provider } from 'react-redux';
-import store from "../redux/store";
 
 export const App = () => {
   return (
@@ -11,11 +11,11 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       <Provider store={store}>
-        <Phonebook/>
+        <Phonebook />
       </Provider>
     </div>
   );
