80367fe421b75f8788f8c63f2d62755b00788ff6
MateuszKar2 <system505@protonmail.com>
2024-01-07 23:23:33 +0100

    add file

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,29 +4,25 @@ import styles from './Filter.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
 
-
 const Filter = () => {
-    const filter = useSelector(state => state.filter)
-    const dispatch = useDispatch()
+  const filter = useSelector(state => state.filter)
+  const dispatch = useDispatch()
 
-
-    const handleChangeFilter = e => {
-        dispatch(setFilter(e.target.value))
-    }
-
-    return (
-        <div>
-            <label className={styles.label}>
-                Find contacts by name:
-                <input className={styles.input} type="text" value={filter} onChange={handleChangeFilter} />
-            </label>
-        </div>
-    );
+  const handleChangeFilter = e => {
+    dispatch(setFilter(e.target.value))
+  }
+  return (
+    <div>
+      <label className={styles.label}>
+        Find contacts by name:
+        <input className={styles.input} type="text" value={filter} onChange={handleChangeFilter} />
+      </label>
+    </div>
+  );
 };
 
 Filter.propTypes = {
-    value: PropTypes.string.isRequired,
-    onChangeFilter: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
 };
-
 export default Filter;
